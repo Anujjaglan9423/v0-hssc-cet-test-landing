1cceb3079b8f6729d0401694ec569aff21f9139e
Anujjaglan9423 <147906142+Anujjaglan9423@users.noreply.github.com>
2026-02-26 17:20:49 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -95,12 +95,12 @@ export default function LoginPage() {
             </div>
 
             <div className="space-y-2">
-              <div className="flex items-center justify-between">
+              {/* <div className="flex items-center justify-between">
                 <Label htmlFor="password">Password</Label>
                 <Link href="#" className="text-sm text-primary hover:underline">
                   Forgot password?
                 </Link>
-              </div>
+              </div> */}
               <div className="relative">
                 <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-muted-foreground" />
                 <Input
